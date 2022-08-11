3ac3774a626b9c3a1d4fbd3abbd3446d06e95f51
mlshvch <malashevichdev@gmail.com>
2022-08-11 13:29:18 +0300

    Implement remainingOrders function

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -69,5 +69,10 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  do {
+    let current_order = orders.shift();
+    // @ts-ignore
+    timeLeft -= timeToMixJuice(current_order)
+  } while (timeLeft > 0)
+  return orders
 }
