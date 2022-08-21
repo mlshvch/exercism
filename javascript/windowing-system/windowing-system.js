fde0f0b3fde62e58a0c8aa57df8c903af24b95c5
Aliaksei Malashevich <87980319+mlshvch@users.noreply.github.com>
2022-08-21 23:06:46 +0300

    Dev/windowing_system (#21)
    * Init task
    
    * Implement Size class
    
    * Implement Position class
    
    * Implement ProgramWindow class
    
    * Implement resize method
    
    * Implement move method
    
    * Implement changeWindow function

---
+++ javascript/windowing-system/windowing-system.js
@@ -0,0 +1,63 @@
+// @ts-check
+
+/**
+ * Implement the classes etc. that are needed to solve the
+ * exercise in this file. Do not forget to export the entities
+ * you defined so they are available for the tests.
+ */
+
+export function Size(width = 80, height = 60) {
+  this.width = width;
+  this.height = height;
+}
+
+Size.prototype.resize = function (newWidth, newHeight) {
+  this.width = newWidth;
+  this.height = newHeight;
+}
+
+export class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y
+  }
+
+  move(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+}
+
+export class ProgramWindow {
+  constructor() {
+    this.size = new Size;
+    this.screenSize = new Size(800, 600);
+    this.position = new Position;
+  }
+
+  resize(newSize) {
+
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    const newWidth = Math.max(1, Math.min(newSize.width, maxWidth))
+    const newHeight = Math.max(1, Math.min(newSize.height, maxHeight))
+
+    this.size.resize(newWidth, newHeight)
+  }
+
+  move(newPosition) {
+    const maxWidth = this.screenSize.width - this.size.width;
+    const maxHeight = this.screenSize.height - this.size.height;
+
+    const newX = Math.max(0, Math.min(newPosition.x, maxWidth))
+    const newY = Math.max(0, Math.min(newPosition.y, maxHeight))
+    this.position.move(newX, newY);
+  }
+}
+
+export function changeWindow(programWindow) {
+  programWindow.size.resize(400, 300);
+  programWindow.position.move(100,150);
+  return programWindow;
+}
