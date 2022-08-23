22b19fa0d1e998909dcad863518ae39871c61bae
mlshvch <malashevichdev@gmail.com>
2022-08-23 14:32:29 +0300

    Implement twoIsSpecial function

--- javascript/elyses-transformative-enchantments/enchantments.js
+++ javascript/elyses-transformative-enchantments/enchantments.js
@@ -64,7 +64,7 @@ export function sandwichTrick(deck) {
  * @returns {number[]} deck with only 2s
  */
 export function twoIsSpecial(deck) {
-  throw new Error('Implement the twoIsSpecial function');
+  return deck.filter((elem) => elem === 2);
 }
 
 /**
