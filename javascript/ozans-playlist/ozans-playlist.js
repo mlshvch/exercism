92ec2af4d12c15580b3218ad0f3766b875f8b539
Aliaksei Malashevich <87980319+mlshvch@users.noreply.github.com>
2022-08-23 15:54:33 +0300

    Dev/ozans_playlist (#25)
    * Init task
    
    * Implement removeDuplicates function
    
    * Implement hasTrack function
    
    * Implement addTrack function
    
    * Implement deleteTrack function
    
    * Implement listArtists function

---
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -0,0 +1,78 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(playlist) {
+  let set = new Set();
+  let result = [];
+  for (let item of playlist) {
+    set.add(item);
+  }
+  set.forEach((item) => result.push(item))
+  return result;
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return playlist.includes(track);
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  let set = new Set();
+  set.add(track);
+  playlist.forEach((item) => set.add(item));
+  let result = [];
+  set.forEach((item) => result.push(item));
+  return result;
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  playlist.forEach((item, index) => {
+    if (item === track) {
+      playlist.splice(index, 1)
+    }
+  });
+  return playlist;
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  let set = new Set;
+  let result = [];
+  playlist.forEach((item) => set.add(item.split(' - ')[1]));
+  set.forEach((item) => result.push(item));
+  return result;
+}
